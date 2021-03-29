aae950d7732dea5825af9bbe92acb47ad5ebf6be
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2021-03-29 15:03:28 -0300

    public folder

---
+++ public/precache-manifest.40d46eb163575eb2b5c22ab2d2b435a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcacb46cc8c01e09d2b7f10c13f1640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32e5ae52b1ff69275c99",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "93c8d9d22ebe57e7c951",
+    "url": "/static/css/main.f38e7c6f.chunk.css"
+  },
+  {
+    "revision": "32e5ae52b1ff69275c99",
+    "url": "/static/js/2.8ff30d85.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8ff30d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c8d9d22ebe57e7c951",
+    "url": "/static/js/main.bdb9d26b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
